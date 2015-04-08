4246be74d1e2098bf1995db41b756b54ace8d42a
Chetan Surpur <chetan51@gmail.com>
2015-04-07 22:59:24 -0700

    Scrolling correctly

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,22 +6,31 @@ public class Player : MonoBehaviour {
 
 	public Vector2 speed = new Vector2 (3, -1);
 
-	private Vector2 movement = new Vector2 (0, 0);
 	private Rigidbody2D rb;
+	private Vector2 movement = new Vector2 (0, 0);
+	private Vector3 lastPosition;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
 
 		movement.y = speed.y;
+		lastPosition = transform.position;
 	}
 
 	// Update is called once per frame
 	void Update () {
+		// Calculate horizontal movement
 		Vector2 mousePosition = Input.mousePosition;
 		mousePosition.x = Math.Min (Math.Max (mousePosition.x, 0), Screen.width);
 		Vector2 target = Camera.main.ScreenToWorldPoint (mousePosition);
 		movement.x = (target.x - rb.position.x) * speed.x;
+
+		// Update camera position
+		Vector3 position = Camera.main.transform.position;
+		position.y += transform.position.y - lastPosition.y;
+		Camera.main.transform.position = position;
+		lastPosition = transform.position;
 	}
 
 	void FixedUpdate () {
