4d9bdd3092b1c0e66a054bf7c21c47b16d27e938
Chetan Surpur <chetan51@gmail.com>
2015-04-07 22:26:59 -0700

    Hello world!

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Player : MonoBehaviour {
+
+	public Vector2 speed = new Vector2 (3, -1);
+
+	private Vector2 movement = new Vector2 (0, 0);
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+
+		movement.y = speed.y;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector2 mousePosition = Input.mousePosition;
+		mousePosition.x = Math.Min (Math.Max (mousePosition.x, 0), Screen.width);
+		Vector2 target = Camera.main.ScreenToWorldPoint (mousePosition);
+		movement.x = (target.x - rb.position.x) * speed.x;
+	}
+
+	void FixedUpdate () {
+		rb.velocity = movement;
+	}
+}
