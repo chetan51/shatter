dd8354e2a2a27f18c724966c5a51d7adfef68b9d
Chetan Surpur <chetan51@gmail.com>
2015-04-12 15:53:41 -0700

    Initial commit for initial game

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,26 +4,39 @@ using System;
 
 public class Player : MonoBehaviour {
 
-	public Vector2 speed = new Vector2 (3, -1);
+	public Rigidbody2D rb;
+	public Vector2 speed = new Vector2(3, -1);
+	public float stationaryTimeScale = 0.15f;
+	public float movingTimeScale = 1.0f;
 
-	private Rigidbody2D rb;
-	private Vector2 movement = new Vector2 (0, 0);
+	private Vector2 movement = new Vector2(0, 0);
+	private Vector2 target = new Vector2(0, 0);
 	private Vector3 lastPosition;
 
 	// Use this for initialization
 	void Start () {
-		rb = GetComponent<Rigidbody2D> ();
-
 		movement.y = speed.y;
 		lastPosition = transform.position;
 	}
 
 	// Update is called once per frame
 	void Update () {
+		Time.timeScale = stationaryTimeScale;
+
+		if (Input.GetMouseButton(0)) {
+			// Calculate new target
+			Vector2 mousePosition = Input.mousePosition;
+			mousePosition.x = Math.Min (Math.Max (mousePosition.x, 0), Screen.width);
+			target = Camera.main.ScreenToWorldPoint (mousePosition);
+
+			Time.timeScale = movingTimeScale;
+		}
+		else if (Input.GetMouseButton(1)) {
+			// Shoot projectile
+			Debug.Log("shoot");
+		}
+
 		// Calculate horizontal movement
-		Vector2 mousePosition = Input.mousePosition;
-		mousePosition.x = Math.Min (Math.Max (mousePosition.x, 0), Screen.width);
-		Vector2 target = Camera.main.ScreenToWorldPoint (mousePosition);
 		movement.x = (target.x - rb.position.x) * speed.x;
 
 		// Update camera position
