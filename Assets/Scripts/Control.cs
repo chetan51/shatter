c5df752e6b7393d83469332796d3fe0ffc95d40b
Chetan Surpur <chetan51@gmail.com>
2015-04-14 20:21:57 -0700

    Initial control system

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Control : MonoBehaviour {
+
+	public Collider2D colliderComponent;
+	public Missile missile;
+
+	public float movementThreshold = 0.5f;
+
+	private bool isActive;
+	private Vector2 initialInputPosition;
+
+	void Update () {
+		if (Input.GetMouseButton(0)) {
+			Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Vector2 inputPosition = new Vector2(worldPoint.x, worldPoint.y);
+
+			if (Input.GetMouseButtonDown(0)) {
+				isActive = false;
+
+				if (colliderComponent == Physics2D.OverlapPoint(inputPosition)) {
+					initialInputPosition = inputPosition;
+					isActive = true;
+				}
+			}
+
+			if (isActive) {
+				Vector2 delta = inputPosition - initialInputPosition;
+
+				if (delta.magnitude > movementThreshold) {
+					missile.SetTargetBearing(delta);
+				}
+			}
+		}
+	}
+
+}
--- Assets/Scripts/Missile.cs
+++ Assets/Scripts/Missile.cs
@@ -3,13 +3,37 @@ using System.Collections;
 
 public class Missile : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public Rigidbody2D rigidbodyComponent;
+
+	public float speed = 3.0f;
+	public float angularSpeed = 0.1f;
+	public float bearing = Mathf.PI / 2;
+
+	private float targetBearing;
+
+	public void SetTargetBearing(Vector2 delta) {
+		targetBearing = Mathf.Atan2(delta.y, delta.x);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Start() {
+		targetBearing = bearing;
 	}
+
+	void Update() {
+		Vector3 angles = new Vector3(0f, 0f, bearing * Mathf.Rad2Deg - 90);
+		transform.localEulerAngles = angles;
+	}
+
+	void FixedUpdate() {
+		float diffBearing = targetBearing - bearing;
+		if (diffBearing > Mathf.PI) {
+			diffBearing = diffBearing - 2 * Mathf.PI;
+		}
+		bearing = bearing + diffBearing * angularSpeed;
+
+		Vector2 movement = new Vector2(speed * Mathf.Cos(bearing),
+		                               speed * Mathf.Sin(bearing));
+		rigidbodyComponent.velocity = movement;
+	}
+
 }
