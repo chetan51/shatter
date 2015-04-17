390a41917a319cbf4ea346c7e2e44e40fb6d06b0
Chetan Surpur <chetan51@gmail.com>
2015-04-16 21:18:07 -0700

    Camera movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -5,6 +5,8 @@ public class CameraMovement : MonoBehaviour {
 
 	public Camera cam;
 	public GameObject screenEdge;
+	public GameObject[] toFollow;
+	public float distanceBehind = 2.5f;
 
 	void Start() {
 		float height = cam.orthographicSize;
@@ -16,6 +18,29 @@ public class CameraMovement : MonoBehaviour {
 												  new Vector2(width, height),
 												  new Vector2(width, -height),
 												  new Vector2(-width, -height)};
+	}
+
+	void LateUpdate () {
+		GameObject closestObject = null;
+		float closestDistance = float.MaxValue;
+
+		float distance;
+		foreach (GameObject objectToFollow in toFollow) {
+			distance = objectToFollow.transform.position.y;
+			if (distance < closestDistance) {
+				closestDistance = distance;
+				closestObject = objectToFollow;
+			}
+		}
+
+		if (closestObject != null) {
+			if (closestDistance > distanceBehind) {
+				Vector3 position = transform.position;
+				position.y = Mathf.Max(closestObject.transform.position.y + cam.orthographicSize - distanceBehind,
+				                       transform.position.y);
+				transform.position = position;
+			}
+		}
 	}
 
 }
