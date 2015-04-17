87fdad3bfb247bbfb729b1a9f5b59fcde5c6da97
Chetan Surpur <chetan51@gmail.com>
2015-04-16 20:36:37 -0700

    Hitting the edge of the screen is death

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	public Camera cam;
+	public GameObject screenEdge;
+
+	void Start() {
+		float height = cam.orthographicSize;
+		float width = height * cam.aspect;
+
+		EdgeCollider2D colliderComponent = screenEdge.GetComponent<EdgeCollider2D>();
+		colliderComponent.points = new Vector2[] {new Vector2(-width, -height),
+												  new Vector2(-width, height),
+												  new Vector2(width, height),
+												  new Vector2(width, -height),
+												  new Vector2(-width, -height)};
+	}
+
+}
