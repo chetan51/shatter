9e33b607cd475c0522c702c8ef45502c6202bc07
Chetan Surpur <chetan51@gmail.com>
2015-04-24 17:50:30 -0700

    Started prototyping new projectile

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	public Rigidbody2D rigidbodyComponent;
+
+	public void AddForce(Vector2 force) {
+		rigidbodyComponent.AddForce(force);
+		rigidbodyComponent.angularVelocity = force.magnitude * 100;
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		Target target = collision.gameObject.GetComponent<Target>();
+		if (target != null && target.missile == this.gameObject) {
+			return;
+		}
+
+    	Application.LoadLevel(Application.loadedLevel);
+	}
+
+}
---
+++ Assets/Scripts/ProjectileControl.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileControl : MonoBehaviour {
+
+	public SpriteRenderer spriteRenderer;
+	public LineRenderer lineRenderer;
+	public Collider2D colliderComponent;
+	public Projectile projectile;
+
+	private bool isActive;
+	private bool isControllable = true;
+	private Vector2 startInputPosition;
+	private Vector2 endInputPosition;
+
+	private void UpdateTrace() {
+		if (isActive) {
+			lineRenderer.SetVertexCount(2);
+			lineRenderer.SetPosition(0, startInputPosition);
+			lineRenderer.SetPosition(1, endInputPosition);
+		}
+		else {
+			lineRenderer.SetVertexCount(0);
+		}
+	}
+
+	void Start() {
+		Color startColor = spriteRenderer.color;
+		Color endColor = startColor;
+		endColor.a = 0f;
+		lineRenderer.SetColors(startColor, endColor);
+	}
+
+	void Update () {
+		if (isActive && Input.GetMouseButtonUp(0)) {
+			projectile.AddForce(endInputPosition - startInputPosition);
+			isActive = false;
+			isControllable = false;
+		}
+
+		if (isControllable && Input.GetMouseButton(0)) {
+			Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Vector2 inputPosition = new Vector2(worldPoint.x, worldPoint.y);
+
+			if (Input.GetMouseButtonDown(0)) {
+				if (colliderComponent == Physics2D.OverlapPoint(inputPosition)) {
+					startInputPosition = inputPosition;
+					isActive = true;
+				}
+			}
+
+			if (isActive) {
+				endInputPosition = inputPosition;
+			}
+		}
+
+		UpdateTrace();
+	}
+
+}
